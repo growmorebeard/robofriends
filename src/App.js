18af99decc569d84e7eedb2e44770036820b85af
John Michael Vicmudo <growmorebeard@users.noreply.github.com>
2022-07-06 08:31:11 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Searchbox from "./components/Searchbox";
+import Cardlist from "./components/Cardlist";
+import Scroll from "./components/Scroll";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const [filterName, setFilterName] = useState("");
+  const [robots, setRobots] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/users").then(res => {
+      setRobots(res.data);
+      console.log(robots);
+    });
+  }, []);
+
+  const onSearchChange = event => {
+    setFilterName(event.target.value);
+  };
+
+  const filteredRobots = robots.filter(robot => {
+    return robot.name.toLowerCase().includes(filterName.toLowerCase());
+  });
+
+  if (!robots.length) {
+    return <h1>Loading...</h1>;
+  } else {
+    return (
+      <div className="tc">
+        <h1 className="f2">Robofriends</h1>
+        <Searchbox searchChange={onSearchChange} />
+        <Scroll>
+          <Cardlist robots={filteredRobots} />
+        </Scroll>
+      </div>
+    );
+  }
 }
-
-export default App;
